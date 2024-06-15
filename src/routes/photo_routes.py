fcd418f51f01145273a35af1c22de719554a62c5
윤주 <nyj8679@gmail.com>
2024-06-15 22:57:59 +0900

    Update photo_routes.py

--- src/routes/photo_routes.py
+++ src/routes/photo_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, session, render_template, request, redirect
 from src.models.photo import Photo
+from src.models.user import User
 
 photo_bp = Blueprint('photo_bp', __name__)
 
@@ -21,9 +22,7 @@ def upload_photo():
 @photo_bp.route('/search', methods=['GET'])
 def search_photos_by_keyword():
     keyword = request.args.get('keyword', '')
-
     photos = Photo.search_by_keyword(keyword)
-    print(photos)
     
     return render_template('main-loged-in.html', photos=photos)
 
@@ -37,6 +36,21 @@ def uploadpost_page():
         return jsonify({"message": "인증 실패"}), 401
     return render_template('upload-post.html')
 
+@photo_bp.route('/update', methods=['POST'])
+def update_photo():
+    if 'userid' not in session:
+        return jsonify({"message": "인증 실패"}), 401
+    
+    photo_id = request.form['photo_id']
+    description = request.form['description']
+    keywords = request.form.get('keywords', '').split(',')
+    image_file = request.files.get('image')
+
+    # 클래스 메소드 호출
+    Photo.update(photo_id, description, keywords, image_file)
+
+    return redirect('/users/mypage')
+
 
 @photo_bp.route('/otherpost', methods=['GET'])
 def otherpost():
@@ -46,5 +60,6 @@ def otherpost():
 def mypost():
     photo_id = request.args.get('id')
     photo = Photo.get_photos_by_photoId(photo_id)
-    print(photo)
-    return render_template('my-post.html', photo=photo)
+    userid = session['userid']
+    username = User.get_username_by_userid(userid)
+    return render_template('my-post.html', photo=photo, username=username)
